2993bed0ac47f7318f75f43852bcbee2171c1f90
abhiram <abhiram@bluejeans.com>
2015-03-09 12:54:22 +0530

    Modified formatting to follow PEP-8 standards

--- getIeeeLinks.py
+++ getIeeeLinks.py
@@ -1,22 +1,22 @@
 from BeautifulSoup import BeautifulSoup
 import urllib2
-import re
 
-inputmsg=raw_input("Enter search term:")
-url1=""
-for i in inputmsg.split():
-    url1=url1+i+"+"
-url2=url1[:-1]
-urlsearch="http://ieeexplore.ieee.org/search/searchresult.jsp?newsearch=true&queryText="+url2
-tempurl="http://ieeexplore.ieee.org/search/searchresult.jsp?newsearch=true&queryText=natural+language+processing+cloud+computing"
-html_page=urllib2.urlopen(urlsearch)
-soup=BeautifulSoup(html_page)
-links=[]
+input_msg = raw_input("Enter search term:")
+url1 = ""
+for i in input_msg.split():
+    url1 = url1+i+"+"
+url2 = url1[:-1]
+url_search = "http://ieeexplore.ieee.org/search/searchresult.jsp?newsearch=true&queryText="+url2
+temp_url0 = "http://ieeexplore.ieee.org/search/searchresult.jsp?"
+temp_url = temp_url0 + "newsearch=true&queryText=natural+language+processing+cloud+computing"
+html_page = urllib2.urlopen(url_search)
+soup = BeautifulSoup(html_page)
+links = []
 
 for link in soup.findAll('a'):
     links.append(link.get('href'))
 
 for i in links:
-    if i!=None:
-        if i[:12]=="/xpl/article":
+    if i is not None:
+        if i[:12] == "/xpl/article":
             print "http://ieeexplore.ieee.org"+i
